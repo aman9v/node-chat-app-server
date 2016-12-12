f3a8404e72010d6438b9aaefdcd74c167a96efda
nglenno18 <nglenno18@gmail.com>
2016-12-12 11:03:33 -0600

    Add a Form for messages, Show incoming messages in Browser

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,8 @@
 //CLIENT-SIDE javascript
 var socket = io();
 socket.on('connect', function(){
-  console.log('Connected to the Server (this message was sent from index.html)');
+  console.log(socket.id);
+  console.log(`NEW CONNECTION (this message was sent from index.html) \n\t CLIENT: ${socket.id}`);
 
   // socket.emit('createEmail', {to: "julie@sdf.com", text: "event emitted from browser console"});
         //^^can be entered through the browser console to the server console
@@ -11,19 +12,38 @@ socket.on('connect', function(){
 });
 
 socket.on('disconnect', function(){
-  console.log('DISCONNECTED from the Server');
+  console.log(`CLIENT: ${socket.id} --> DISCONNECTED from Server`);
 });
 
-//listen to a CUSTOM event
-//emit event from server, sending data to the client
-socket.on('newEmail', function(email){
-  var em = email;
-  //template strings dont work //console.log(`\nCUSTOM EVENT: newEmail callback function is fired\n\t${em}`);
-  console.log('New Email', email);
-});
 
+//EVENT Acknowledgements
+// socket.emit('createMessage', {
+//   from: `Frank`,
+//   text: 'Event Emitter added for Event Acknowledgement functionality'
+// }, function(callbackArg){
+//   console.log(`Got IT!  `, callbackArg);
+// });
 
 //CHALLENGE newMessage
 socket.on('newMessage', function(message){
   console.log('New Message', message);
-})
+
+  //create a list item on jquery to CREATE and MODIFY an element before rendering to markup
+  var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`); //created an element but have not rendered it to the dom
+    jQuery('#messages').append(li);
+});
+
+
+//call jQuery with the selector and an event listener
+    //--listener should be calling on, and providing 2 args (eventName, function(){})
+jQuery('#message-form').on('submit', function(eventArgument){
+  eventArgument.preventDefault();   //stop the submit event from firing, now nothing happens
+
+  socket.emit('createMessage', {
+    from: `User: ${socket.id}`,
+    text: jQuery('[name=message]').val()
+  }, function(){//add callback function
+
+  });
+});
